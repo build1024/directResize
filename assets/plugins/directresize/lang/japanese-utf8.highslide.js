6922d5b58d3cdea7bcc512c49599b9afcdaabac3
hira <elt@every-little.com>
2018-12-11 21:01:05 +0900

    add Japanese-utf8 language files

---
+++ assets/plugins/directresize/lang/japanese-utf8.highslide.js
@@ -0,0 +1,5 @@
+hs.restoreTitle = "クリックで拡大画像を閉じます。ドラッグで移動できます。左右キーで画像を切り替えます。";
+hs.loadingText = "読み込み中...";
+hs.loadingTitle = "クリックでキャンセルします";
+hs.focusTitle = "クリックで前面に表示";
+hs.fullExpandTitle = "画像をさらに拡大";
---
+++ assets/plugins/directresize/lang/japanese-utf8.inc.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Title: Language File
+ * Purpose:
+ *  Japanese language file for DirectResize
+*/
+$_lang['lang'] = "ja";
+$_lang['filesize_b'] = "バイト";
+$_lang['filesize_Kb'] = "KB";
+$_lang['filesize_Mb'] = "MB";
+$_lang['filesize_Tb'] = "TB";
+?>
